5a3ad92dcf164b9786a97d19ed7216371df1e8e8
Krishan <krishankumar.safi@technoidentity.com>
2024-11-16 21:34:00 +0530

    add-mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,14 @@
 import express from "express";
 
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Brainy!");
+});
+
+app.listen("8080", () => {
+  console.log("server is running");
+});
